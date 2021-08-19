1328c6f992db299f6e3edfb719d5aa3a0102d4c1
Angel711 <arubio15@alumnos.uaq.mx>
2021-08-18 20:47:56 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+/*Verbos HTTP\
+GET
+POST
+PATCH
+PUT
+DELETE
+*/
+app.get("/",(rec,res, next) => {
+    res.status(200);
+    res.send("Bienvenido");
+});
+app.listen(3000, () => {
+    console.log('Server is running...');
+});
