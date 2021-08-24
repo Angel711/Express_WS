ee0d6bbb6af08b5f411259148cf35324fc182cee
Angel711 <arubio15@alumnos.uaq.mx>
2021-08-24 09:10:55 -0500

    add the rec.param and /:name to the index

--- index.js
+++ index.js
@@ -12,6 +12,13 @@ app.get("/",(rec,res, next) => {
     res.status(200);
     res.send("Bienvenido");
 });
-app.listen(3000, () => {
+
+app.get("/:name",(rec,res, next) => {
+    console.log(rec.params.name);
+    res.status(200);
+    res.send("Hola "+ rec.params.name);
+})
+
+app.listen(process.env.port || 3000, () => {
     console.log('Server is running...');
 });
