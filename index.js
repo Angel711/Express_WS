6264d58d835c4bdd4cc6cef930bfd8767121c3a3
Angel711 <arubio15@alumnos.uaq.mx>
2021-08-26 10:42:16 -0500

    changed thing on the code to make it more clear and ready to use more difficult things in the future

--- index.js
+++ index.js
@@ -3,49 +3,49 @@ const app = express();
 const {pokemon} = require('./pokedex.json');
 
 /*Verbos HTTP\
-GET
-POST
-PATCH
-PUT
-DELETE
+GET - obtener recursos
+POST - almacenar/crear recursos
+PATCH - modificar una parte de un recurso
+PUT - modificar un recurso
+DELETE - borrar un recurso
 */
-app.get("/",(req,res, next) => {
-    res.status(200);
-    res.send("Welcome to the Pokedex");
-});
 
-app.get("/pokemon/all",(req,res, next) => {
+//Pagina inicial
+app.get("/",(req,res, next) => {
+    return res.status(200).send("Welcome to the Pokedex");
+});
+//Mostrar todos los pokemon
+app.get("/pokemon",(req,res, next) => {
     console.log(req.params.name);
-    res.status(200);
-    res.send(pokemon);
+    return res.status(200).send(pokemon);
 })
+//Buacar pokemon por numero
 app.get('/pokemon/:id([0-9]{1,3})',(req,res,next) => {
     const id = req.params.id - 1;
     if(id >= 0 && id <=150)
     {
-        res.status(200);
-        res.send(pokemon[req.params.id-1]);
+        return res.status(200).send(pokemon[req.params.id-1]);
     }
-    else
-    {
-        res.status(404);
-        res.send("This Pokemon doesn't exist");
-    }
+    return res.status(404).send("This Pokemon doesn't exist");
     
 })
-
-app.get('/pokemon/:name',(req,res, next)=>{
+//Buscar pokemon por nombre
+app.get('/pokemon/:name([A-Za-z]+)',(req,res, next)=>{
     const name = req.params.name;
-    for(i=0;i<pokemon.length;i++)
+
+    const pk = pokemon.filter((p)=>
     {
-        if(pokemon[i].name == name)
-        {
-            res.status(200);
-            res.send(pokemon[i]);
-        }
-    }
-    res.status(404);
-    res.send("This Pokemon doesn't exist");
+    //Operador terniario
+    //condicion ? valor si verdadero : valor si falso
+        return (p.name.toUpperCase()==name.toUpperCase()) ?  p : null;
+        
+    })
+
+    //Operador terniario
+    //condicion ? valor si verdadero : valor si falso
+    (pk.length > 0) ? 
+        res.status(200).send(pk) : 
+        res.status(404).send("This Pokemon doesn't exist");
 })
 
 app.listen(process.env.port || 3000, () => {
