304c2cdabe0b25dca2b8fbc9bad829e5c093a3fd
Angel <aner.0711001@gmail.com>
2021-10-07 07:33:04 -0500

    changed port

--- config/database.js
+++ config/database.js
@@ -1,14 +1,13 @@
 const mysql = require('mysql');
 const util = require('util');
 
-const pool = mysql.createPool(
-    {
-        connectionLimit: 10,
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'pokemon'
-    });
+const pool = mysql.createPool({
+    connectionLimit: 10,
+    host: '0.0.0.0',
+    user: 'root',
+    password: '',
+    database: 'pokemon'
+});
 
 
 pool.query = util.promisify(pool.query);
